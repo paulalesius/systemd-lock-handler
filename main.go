1bdff567e0c46595823a0c6b9b155e4f0fc9cab6
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-09-23 19:56:13 +0200

    Fix bogus log message

--- main.go
+++ main.go
@@ -113,7 +113,7 @@ func ListenForLock(user *user.User) {
 			if name.Value() != user.Username {
 				continue
 			}
-			log.Println("Session locked for current user.")
+			log.Println("Session signal for current user: {}.", signalName)
 
 			err = StartSystemdUserUnit(target)
 			if err != nil {
